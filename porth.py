a26e1ea63fb2f0bccee2d3475c379c67e495f27d
pwnker <tilen.okretic@gmail.com>
2024-03-28 15:30:38 +0100

    feat: `equal` operation

--- porth.py
+++ porth.py
@@ -16,6 +16,7 @@ def iota(reset=False):
 OP_PUSH=iota();
 OP_PLUS=iota();
 OP_MINUS=iota();
+OP_EQUAL=iota();
 OP_DUMP=iota();
 COUNT_OPS=iota();
 
@@ -28,6 +29,9 @@ def plus():
 def minus():
     return (OP_MINUS,);
 
+def equal():
+    return (OP_EQUAL,);
+
 def dump():
     return (OP_DUMP,);
 
@@ -35,7 +39,7 @@ def dump():
 def simulate_program(program):
     stack = [];
     for op in program:
-        assert COUNT_OPS == 4, "Exhaustive handling of operations in simulation"
+        assert COUNT_OPS == 5, "Exhaustive handling of operations in simulation"
         if op[0] == OP_PUSH:
             stack.append(op[1]);
         elif op[0] == OP_PLUS:
@@ -46,6 +50,10 @@ def simulate_program(program):
             a = stack.pop();
             b = stack.pop();
             stack.append(b - a);
+        elif op[0] == OP_EQUAL:
+            a = stack.pop();
+            b = stack.pop();
+            stack.append(int(a == b));
         elif op[0] == OP_DUMP:
             a = stack.pop();
             print(a);
@@ -88,7 +96,7 @@ def compile_program(program, out_file_path):
         out.write("global _start\n");
         out.write("_start:\n");
         for op in program:
-            assert COUNT_OPS == 4, "Exhaustive handling of operations in compilation"
+            assert COUNT_OPS == 5, "Exhaustive handling of operations in compilation"
             if op[0] == OP_PUSH:
                 out.write(";;  -- push %d --\n" % op[1]);
                 out.write("    push %d\n" % op[1]);
@@ -104,6 +112,15 @@ def compile_program(program, out_file_path):
                 out.write("    pop rbx\n");
                 out.write("    sub rbx, rax\n");
                 out.write("    push rbx\n");
+            elif op[0] == OP_EQUAL:
+                out.write(";;  --  equal --\n");
+                out.write("    mov rcx, 0\n");
+                out.write("    mov rdx, 1\n");
+                out.write("    pop rax\n");
+                out.write("    pop rbx\n");
+                out.write("    cmp rax, rbx\n");
+                # move 1 to rcx when rax == rbx
+                out.write("    cmove rcx, rdx\n");
             elif op[0] == OP_DUMP:
                 out.write(";;  -- dump %d --\n");
                 out.write("    pop rdi\n");
@@ -116,11 +133,13 @@ def compile_program(program, out_file_path):
 
 def parse_token_as_op(token):
     (file_path, row, col, word) = token;
-    assert COUNT_OPS == 4, "Exhaustive op handling in parse_token_as_op";
+    assert COUNT_OPS == 5, "Exhaustive op handling in parse_token_as_op";
     if word == '+':
         return plus();
     elif word == '-':
         return minus();
+    elif word == '=':
+        return equal();
     elif word == '.':
         return dump();
     else: 
@@ -195,6 +214,6 @@ if __name__ == '__main__':
         call_cmd(["nasm", "-f", "elf64", "./build/output.asm"]);
         call_cmd(["ld", "-o", "output", "./build/output.o"]);
     else:
-        print_usage();
+        print_usage(program_name);
         print("ERROR: unknown subcommand '%s'" % (subcommand));
         exit(1);
