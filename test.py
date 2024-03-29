1118a772797ee5f34da4e7cb4c8621a302968a84
pwnker <tilen.okretic@gmail.com>
2024-03-29 11:54:24 +0100

    feat:test files and script that runs all tests

---
+++ test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import subprocess;
+
+if __name__ == '__main__':
+    subprocess.run(["./porth.py", "sim", "./test/01-arithemtics.porth"]);
+    subprocess.run(["./porth.py", "sim", "./test/02-conditions.porth"]);
+    subprocess.run(["./porth.py", "sim", "./test/03-loops.porth"]);
