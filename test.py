9b37e218776d2c66ad32776b1866c56e6e003910
pwnker <tilen.okretic@gmail.com>
2024-03-30 11:22:20 +0100

    update: playing with testing file

--- test.py
+++ test.py
@@ -5,3 +5,4 @@ if __name__ == '__main__':
     subprocess.run(["./porth.py", "sim", "./test/01-arithemtics.porth"]);
     subprocess.run(["./porth.py", "sim", "./test/02-conditions.porth"]);
     subprocess.run(["./porth.py", "sim", "./test/03-loops.porth"]);
+    subprocess.run(["./porth.py", "sim", "./test/04-memory.porth"]);
