5521e6547a114d743ef9a54c52a8c908862f85ba
pwnker <tilen.okretic@gmail.com>
2024-04-17 15:50:36 +0200

    update: better debug comments in the generated assembly

--- porth.py
+++ porth.py
@@ -85,7 +85,7 @@ class Intrinsic(IntEnum):
     SYSCALL5=auto()
     SYSCALL6=auto()
 
-class OpType(IntEnum):
+class OpType(Enum):
     PUSH_INT=auto()
     PUSH_STR=auto()
     PUSH_CSTR=auto()
@@ -1117,7 +1117,7 @@ def generate_nasm_linux_x86_64(program: Program, out_file_path: str):
             op = program.ops[ip]
             assert len(OpType) == 16, "Exhaustive ops handling in generate_nasm_linux_x86_64: %d" % len(OpType)
             out.write("addr_%d:\n" % ip)
-            out.write(";;  -- %s --\n" % op)
+            out.write(";;  -- %s:%d:%d: %s (%s) --\n" % (op.token.loc + (repr(op.token.text), op.typ)))
             if op.typ == OpType.PUSH_INT:
                 assert isinstance(op.operand, int), "There is a bug in the parsing step (probably)"
                 out.write("    mov rax, %d\n" % op.operand)
